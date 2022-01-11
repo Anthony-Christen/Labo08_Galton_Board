5b28031950f02b360d072e49992ec7f24c5a3ca9
Anthony Christen <anthony1998@bluewin.ch>
2022-01-11 15:53:31 +0100

    initialisation du projet

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+  -----------------------------------------------------------------------------------
+  Fichier     : main.cpp
+  Nom du labo : Labo08_Galton_Board
+  Auteur(s)   : Kylian Manzini & Anthony Christen
+  Date        : 11.01.2021
+  But         : le but du programme et non le but du laboratoire !!
+
+  Remarque(s) : à compléter
+
+  Compilateur : Apple clang version 13.0.0 (clang-1300.0.29.3)
+  -----------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   return EXIT_SUCCESS;
+}
